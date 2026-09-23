40f0d310af5e149ef00a2cbb5fb47c15881f708d
Mohammed-Ahmed-Ezzat <mohammedahmedezzat.2004@gmail.com>
2026-09-23 03:20:19 +0300

    chore: remove redundant files, duplicate assets, and clean up repository

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 3000,
     open: true,
     watch: {
-      ignored: ['**/dist/**', '**/*.pptx', '**/index.vanilla.html']
+      ignored: ['**/dist/**']
     }
   },
   preview: {
